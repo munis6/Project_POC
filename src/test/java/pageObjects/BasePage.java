47d6c500dfacb317fb4b2cc4c314d4563fb0f4d9
muni <deals4sekhar@gmail.com>
2024-12-08 12:16:58 -0500

    today_12_8_2024

--- src/test/java/pageObjects/BasePage.java
+++ src/test/java/pageObjects/BasePage.java
@@ -3,11 +3,11 @@ package pageObjects;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
-public class BasePage {
+class BasePage {
 	
-	WebDriver driver;
+	protected WebDriver driver;
 	
-	public BasePage(WebDriver driver)
+	 BasePage(WebDriver driver)
 	{
 		this.driver=driver;
 		PageFactory.initElements(driver, this); //this is very important to load page objects
